bc77855a3da5c2fffd68b7de34e40faf45bda31c
James Katarikawe <jpkat92@gmail.com>
2018-02-13 15:38:16 +0300

    Code organised into models, views and octopus using the MVO design pattern

--- js/app.js
+++ js/app.js
@@ -1,39 +1,118 @@
-var listItems = $("#catList > li");
-var cats = $(".cat");
+$(function() {
 
-function hideAllCats(){
-    for(var i = 0; i < cats.length; i++){
-        $(cats[i]).hide();
-    }
-}
+    var model = {
+        cats : [
+        {
+            name: "Fat cat",
+            imgUrl : "fat-black-cat.svg",
+            clickCount : 0
+        },
+         {
+            name: "Heath Cliff",
+            imgUrl: "heathcliff.png",
+            clickCount: 0
+        },
+        {
+            name: "Garfield",
+            imgUrl: "garfield.svg",
+            clickCount: 0
+        },
+        {
+            name: "Tom",
+            imgUrl: "tom-cat.png",
+            clickCount: 0
+        },
+        {
+            name: "Sylvester",
+            imgUrl: "sylvester-cat.png",
+            clickCount: 0
+        }],
+        getCats: function(){
+            return this.cats;
+        },
+        getCat: function(index){
+            return this.cats[index]
+        },
+        setActiveCat: function(cat){
+            this.activeCat = cat;
+        },
+        activeCat : null
+    };
+    
+    var octopus = {
+        init: function(){
+            model.setActiveCat(model.cats[0]);
+            catListView.render();
+            catDetailView.init();           
+        },
+        getAllCats: function(){
+            return model.getCats();
+        },
+        getActiveCat: function(){
+            return model.activeCat;
+        },        
+        increaseCount: function(){
+            model.activeCat.clickCount++; 
+            catDetailView.render();
+        } 
+    };
 
-function bindCatPhotos(id){
-    var cat = "#cat"+id;
-    $(cat).click(function(){
-		var counter = $(cat+" .count").text();
-        counter = parseInt(counter) +1 ;
-        $(cat+" .count").text(counter);
-	})
-}
+    var catListView = {
+        render: function(){
+            var catList = document.getElementById('catList');
+            var cats = octopus.getAllCats();
+            
+            catList.innerHTML = "";
+            
+            //iterate over cats
+            for(var i=0;i<cats.length;i++){
+                var cat = cats[i];
+                // create a cat html list item
+                var catElement = document.createElement("li");
+                catElement.textContent = cat.name;
+                
+                catElement.addEventListener("click", (function(cat){
+                    return function(){
+                        model.setActiveCat(cat);
+                        catDetailView.render();
+                    };
+                })(cat));
+                
+                // Add list item html to list
+                catList.appendChild(catElement);
+            }
+        }
+    };
+    
+     var catDetailView = { 
+        init: function(){            
+            this.name = document.getElementsByClassName("cat-name")[0];  
+            this.pic = document.getElementsByClassName("cat-pic")[0];
+            this.count = document.getElementsByClassName("count")[0];
+            
+            this.pic.addEventListener("click", function(){
+                octopus.increaseCount(); 
+            });
+            
+            this.render();
+        },
+        render: function(){
+            // Update DOM Elements with selected cat
+            var catData = model.activeCat;    
+            this.clear();
+            this.name.textContent = catData.name;
+            this.pic.src = catData.imgUrl;
+            this.count.textContent = catData.clickCount;
+            
+            
+        },
+         clear: function(){
+             // Clear DOM elements of content
+            this.name.innerHTML = "";
+            this.pic.src = "";
+            this.count.innerHTML = "";
+        }
+    };
 
-function bindListItemToCat(id){
-    $("#catItem"+id).click(function(){
-		hideAllCats();
-        var catName = $("#catItem"+id).text();
-        $("#cat"+id+">.cat-name").text(catName);
-		$("#cat"+id).show();
-	})
-}
-
-for(var i=0;i<cats.length;i++){
-    bindListItemToCat(i);
-}
-
-for(var i=0;i<cats.length;i++){
-    bindCatPhotos(i);
-}
-
-hideAllCats();
-var catName = $("#catItem1").text();
-$("#cat1>.cat-name").text(catName);
-$("#cat1").show();
+    octopus.init();
+});
